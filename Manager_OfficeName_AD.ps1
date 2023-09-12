23a6cdac6ef42ad018edec4adcabb38b832b87c6
andersonldmota <93939608+andersonldmota@users.noreply.github.com>
2023-09-12 15:20:34 -0300

    Add files via upload

---
+++ Manager_OfficeName_AD.ps1
@@ -0,0 +1,6 @@
+﻿## Cria uma variavael com o conteudo do arquivo CSV
+$CSV = Import-Csv -path "d:\Scripts\Atributos Gerentes AD\manager_ad.csv"
+
+
+## %%%Aplicar alterações no AD, baseado no arquivo CSV montado ##
+$CSV | ForEach-Object { Set-ADUser -identity $_.sAMAccountName -Manager $_.Manager }
